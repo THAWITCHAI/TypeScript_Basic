c8c451f5f1b95d6905bd4337d76c43dbcb72d708
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-04-18 03:05:09 +0700

    Create Data_Type.ts

---
+++ Data_Type.ts
@@ -0,0 +1,10 @@
+let myName: String = "Thawitchai";
+console.log(myName)
+console.log(typeof(myName))
+
+let money:number = 1500;
+console.log(money)
+console.log(typeof(money))
+
+let mindSet:undefined;
+console.log(mindSet)
