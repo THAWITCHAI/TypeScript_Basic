60793e54cf6859e5d3ef8ca69b79514b79d24120
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-04-18 16:39:01 +0700

    Update Data_Type.ts

--- Data_Type.ts
+++ Data_Type.ts
@@ -1,10 +1,21 @@
+var myAge: number;
+var myBool: boolean;
+var count: number;
+var i;
+
 let myName: String = "Thawitchai";
-console.log(myName)
-console.log(typeof(myName))
+console.log(myName);
+console.log(typeof myName);
 
-let money:number = 1500;
-console.log(money)
-console.log(typeof(money))
+let money: number = 1500;
+console.log(money);
+console.log(typeof money);
 
-let mindSet:undefined;
-console.log(mindSet)
+const sayHello = () => {
+  if (true) {
+    let name = "Thawitchai";
+    var age = 15;
+  }
+//   console.log(name);
+  console.log(age);
+};
